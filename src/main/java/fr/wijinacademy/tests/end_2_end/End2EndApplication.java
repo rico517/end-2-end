bafba1b1400c4cb5158b79955cab2d01f5e6cd60
eric.girard <delasalle.sio.girard.e@gmail.com>
2025-03-19 09:39:55 +0100

    first commit

---
+++ src/main/java/fr/wijinacademy/tests/end_2_end/End2EndApplication.java
@@ -0,0 +1,13 @@
+package fr.wijinacademy.tests.end_2_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class End2EndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(End2EndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/wijinacademy/tests/end_2_end/End2EndApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.wijinacademy.tests.end_2_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class End2EndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
